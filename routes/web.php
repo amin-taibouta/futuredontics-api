96bc530eec9205bb267024b590b522b7589060a0
Amin Taibouta <amin.taibouta@gmail.com>
2022-06-23 18:52:11 +0100

    change sp name

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/ping', function(Request $request) {
     */
 
     //fetch date
-    $return = DB::select("call display_message('Hello')");
+    $return = DB::select("call dbo.DTP_ProcessCall('99999')");
     //log the request
     Log::info("API Response", [$return]);
     //return the data
